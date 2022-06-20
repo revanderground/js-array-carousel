eeec8e75fb2e3a29fc802f5e34b76327874be5d1
vtnboolean <vnthuanx@gmail.com>
2022-06-20 17:47:39 +0200

    first step

--- js/main.js
+++ js/main.js
@@ -1,34 +1,12 @@
-// Prendo tutte le immagini figlie di carousel-image-container
-//le salvo in un array (o come se lo fosse)
 
-// devo aggiungere l'event listener:
-//al bottone next, sul click, viene aggiunta la classe active sull'immagine successiva
-//dobbiamo rimuovere l'active su quella precedente
+const images = [
+    'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
+    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
+    'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
+    'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg',
+    'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
+    'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
+    ];
 
-let activeElementIndex=1;
+let image = document.createElement('div');
 
-const imgsWrapper = document.querySelector('carousel-wrapper .carousel-image-container');
-const imgList = imgsWrapper.children;
-
-imgList[activeElementIndex].classList.add('active');
-
-//quando premo un bottone, prendo l'immagine attuale e la nascondo e mostro la successiva
-
-const btnNext = document.getElementById('next-button');
-
-btnNext.addEventListener('click', function(){
-    //rimuovo l'immagine attuale
-imgList[activeElementIndex].classList.remove('active');
-//scorro di un posto l'elemento attivo
-activeElementIndex++;
-
-if(activeElementIndex ==imgList.length){
-    activeElementIndex=0;
-}
-
-//aggiungo la classe active al nuovo elemento attivo
-// imgList[activeElementIndex].classList.add('active');
-
-document.querySelectorAll('.carousel-image-container img')[activeElementIndex].classlist.add('active');
-console.log('L\'elemento adesso attivo è in posizione: ' + activeElementIndex);
-});
